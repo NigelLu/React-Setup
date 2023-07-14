a508beed152e7f5896fcffe45c77115bcda39fc0
Xiaochen Lu <xiaoclu@ebay.com>
2023-07-14 17:04:39 +0800

    add: react useState count-down demo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <h1>Welcome to the demo for ReactJS</h1>;
+  // #region ViewModel Logic
+  const [counter, setCounter] = useState(10);
+  useEffect(() => {
+    const reduceCounter = setInterval(
+      () => setCounter((currentCounter) => currentCounter - 1),
+      1000,
+    );
+    setTimeout(() => clearInterval(reduceCounter), 10000);
+  }, []);
+  // #endregion ViewModel Logic
+
+  // #region View
+  return (
+    <div style={{ marginLeft: "50px" }}>
+      <h1>Welcome to the demo for ReactJS</h1>
+      <h2>{counter ? <>We are counting down:&nbsp;</> : <>Counted down completed!!!</>}</h2>
+      <div style={{ fontSize: "2rem" }}>
+        <span style={{ color: counter ? "orange" : "red" }}>{counter}</span>
+        <span>&nbsp;second{counter > 1 ? "s" : ""}</span>
+      </div>
+    </div>
+  );
+  // #endregion View
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
